ee28e076ae314ecf59d1957abba2d9f83054adb3
Jonathan <cuajonathan279@gmail.com>
2021-05-17 20:51:32 -0600

    ci: Events and listeners authenticate user after registration

---
+++ app/Listeners/AuthenticateUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthenticateUser
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $guard = Auth::guard(config('sanctum.guard', 'web'));
+
+        $guard->login($event->user);
+    }
+}
