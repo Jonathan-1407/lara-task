163c0d2f04f71bd9c144bcdeca15695d8d4eb929
Jonathan <cuajonathan279@gmail.com>
2021-05-17 21:09:11 -0600

    ci: Vuex installation and configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,13 @@ import "./bootstrap";
 import Vue from "vue";
 import apolloProvider from "./apollo";
 import router from "./router";
+import store from "./store";
 
 window.Vue = Vue;
 
 const app = new Vue({
     el: "#app",
     apolloProvider,
-    router
+    router,
+    store
 });
---
+++ resources/js/store/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = {
+    state: {},
+    mutations: {},
+    actions: {},
+    getters: {}
+}
+
+export default new Vuex.Store(store);
